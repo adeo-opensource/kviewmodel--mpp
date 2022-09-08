ecf90c9183a410766ba6b5c820c358cb361bcbf5
Artem Slinkin <slartus@gmail.com>
2022-09-08 18:13:34 +0500

    libraries update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    compileSdk = 31
+    compileSdk = 32
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 31
+        targetSdk = 32
         versionCode = 1
         versionName = "1.0"
 
