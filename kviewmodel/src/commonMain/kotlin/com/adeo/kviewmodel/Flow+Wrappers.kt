77a538ff119160844a2ad6992968e87638e88b7e
Artem Slinkin <slartus@gmail.com>
2022-09-09 09:20:39 +0500

    revert moving warch

--- kviewmodel/src/commonMain/kotlin/com/adeo/kviewmodel/Flow+Wrappers.kt
+++ kviewmodel/src/commonMain/kotlin/com/adeo/kviewmodel/Flow+Wrappers.kt
@@ -1,8 +1,26 @@
 package com.adeo.kviewmodel
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.*
 
-public class WrappedStateFlow<T : Any>(private val origin: StateFlow<T>) : StateFlow<T> by origin
+public class WrappedStateFlow<T : Any>(private val origin: StateFlow<T>) : StateFlow<T> by origin {
+    public fun watch(block: (T) -> Unit): Closeable = watchFlow(block)
+}
 
-public class WrappedSharedFlow<T : Any?>(private val origin: SharedFlow<T>) :
-    SharedFlow<T> by origin
+public class WrappedSharedFlow<T : Any?>(private val origin: SharedFlow<T>) : SharedFlow<T> by origin {
+    public fun watch(block: (T) -> Unit): Closeable = watchFlow(block)
+}
+
+private fun <T> Flow<T>.watchFlow(block: (T) -> Unit): Closeable {
+    val context = CoroutineScope(SupervisorJob() + Dispatchers.Main)
+    onEach(block).launchIn(context)
+
+    return object : Closeable {
+        override fun close() {
+            context.cancel()
+        }
+    }
+}
