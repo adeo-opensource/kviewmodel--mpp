626854e470a856d0298961d01525625437ab0935
Artem Slinkin <slartus@gmail.com>
2022-09-09 15:46:05 +0500

    revert model changes

--- kviewmodel/src/commonMain/kotlin/com/adeo/kviewmodel/BaseSharedViewModel.kt
+++ kviewmodel/src/commonMain/kotlin/com/adeo/kviewmodel/BaseSharedViewModel.kt
@@ -1,22 +1,17 @@
 package com.adeo.kviewmodel
 
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.BufferOverflow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.*
 
 public abstract class BaseSharedViewModel<State : Any, Action, Event>(initialState: State) : KViewModel() {
 
     private val _viewStates = MutableStateFlow(initialState)
 
-    private val _viewActions = MutableSharedFlow<Action>(replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+    private val _viewActions = MutableSharedFlow<Action?>(replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
 
     public fun viewStates(): WrappedStateFlow<State> = WrappedStateFlow(_viewStates.asStateFlow())
 
-    public fun viewActions(): WrappedSharedFlow<Action> = WrappedSharedFlow(_viewActions.asSharedFlow())
+    public fun viewActions(): WrappedSharedFlow<Action?> = WrappedSharedFlow(_viewActions.asSharedFlow())
 
     protected var viewState: State
         get() = _viewStates.value
@@ -25,23 +20,11 @@ public abstract class BaseSharedViewModel<State : Any, Action, Event>(initialSta
         }
 
     protected var viewAction: Action?
-        get() = if (_viewActions.replayCache.isNotEmpty()) {
-            _viewActions.replayCache.last()
-        } else {
-            null
-        }
+        get() = _viewActions.replayCache.last()
         set(value) {
-            if (value != null) {
-                _viewActions.tryEmit(value)
-            }
+            _viewActions.tryEmit(value)
         }
 
     public abstract fun obtainEvent(viewEvent: Event)
 
-    /**
-     * Convenient method to perform work in [viewModelScope] scope.
-     */
-    protected fun withViewModelScope(block: suspend CoroutineScope.() -> Unit) {
-        viewModelScope.launch(block = block)
-    }
 }
