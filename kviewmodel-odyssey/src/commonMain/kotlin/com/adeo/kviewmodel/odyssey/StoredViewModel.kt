20a30cd157443836214ce9a5bba43a7d1ca3ddcc
Artem Slinkin <slartus@gmail.com>
2023-03-10 09:55:03 +0500

    custom key

--- kviewmodel-odyssey/src/commonMain/kotlin/com/adeo/kviewmodel/odyssey/StoredViewModel.kt
+++ kviewmodel-odyssey/src/commonMain/kotlin/com/adeo/kviewmodel/odyssey/StoredViewModel.kt
@@ -9,10 +9,11 @@ import ru.alexgladkov.odyssey.compose.local.LocalRootController
 @Composable
 public inline fun <reified T : KViewModel> StoredViewModel(
     noinline factory: @DisallowComposableCalls () -> T,
-    noinline content: @Composable (T) -> Unit
+    viewModelKey: String? = null,
+    noinline content: @Composable (T) -> Unit,
 ) {
     val currentScreen = LocalRootController.current.currentScreen
-    val screenKey = currentScreen.value?.screen?.key ?: return
+    val screenKey = viewModelKey ?: currentScreen.value?.screen?.key ?: return
     val viewModel = remember { ViewModelStore.getOrPut(screenKey, factory) }
     content(viewModel)
 }
